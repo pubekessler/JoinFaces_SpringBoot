d711c22e746bebe4e90b5000e5295aec46545402
daniel <daniel@daniel-G7>
2021-01-19 09:50:03 -0300

    init

---
+++ src/main/java/com/example/demoPrimefaces/DemoPrimefacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoPrimefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPrimefacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoPrimefaces/DemoPrimefacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoPrimefaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPrimefacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
